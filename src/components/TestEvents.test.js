1dcc6c084689a652e6a3833f3424360bc59b6afa
Lucas <lucasportosd@gmail.com>
2020-07-15 00:02:23 -0300

    f

---
+++ src/components/TestEvents.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  render,
+  cleanup,
+  fireEvent,
+  getByTestId,
+} from "@testing-library/react";
+import TestEvents from "./TestEvents";
+import "@testing-library/jest-dom/extend-expect";
+
+afterEach(cleanup);
+
+it("increments counter", () => {
+  const { getByTestId } = render(<TestEvents />);
+
+  fireEvent.click(getByTestId("button-up"));
+
+  expect(getByTestId("counter")).toHaveTextContent("1");
+});
+
+it("decrements counter", () => {
+  const { getByTestId } = render(<TestEvents />);
+
+  fireEvent.click(getByTestId("button-down"));
+
+  expect(getByTestId("counter")).toHaveTextContent("-1");
+});
